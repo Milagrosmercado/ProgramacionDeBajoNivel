2de9057d98cb9a464df6fae3056037d66de8fd7c
Milagrosmercado <milagrosmercado2002@gmail.com>
2023-04-23 20:25:50 -0300

    agregar carpeta nuevamente

---
+++ IntroduccionAlLenguajeC/ejercicio2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//A partir de un listado de números, determinar el máximo.
+
+int main(int argc, char const *argv[])
+{
+    int numeros[] = {1,2,3,4,6};
+    int maximo = numeros[0];
+
+    for (int i = 0; i < sizeof(numeros) / sizeof(int); i++)  // ver porque se divide por sizeof(int)
+    {
+        if (numeros[i] > maximo)
+        {
+            maximo = numeros[i];
+        }
+        
+    }
+
+    printf("Maximo: %d", maximo);
+
+    return 0;
+}
