0d59aae9ef136fa2de37bc046d8d98ad685e04fc
Milagrosmercado <milagrosmercado2002@gmail.com>
2023-05-16 00:00:48 -0300

    lista

--- Lista/lista.c
+++ Lista/lista.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-//estructura de la lista
+// estructura de la lista
 struct Lista
 {
-    int dato;
-    struct Node *next; // puntero al siguiente nodo
+    int dato;               // valor entero
+    struct Node *siguiente; // puntero al siguiente nodo
 };
 
 int contador;
@@ -15,28 +15,23 @@ int contador;
 // se crea la lista y se agregan los nodos
 struct Lista *agregarALaLista(int dato)
 {
-   
+
     // se agrega el nodo y se le asigna su dato
     struct Lista *node = (struct Lista *)malloc(sizeof(struct Lista));
     node->dato = dato;
 
     // indica que lo siguiente es nulo
-    node->next = NULL;
-    contador++;
-    printf("%d \n",dato);
+    node->siguiente = NULL;
+    contador++;            // para saber cuantos nodos hay
+    printf("%d \n", dato); // imprime cada vez que se agrega un dato
     return node;
 }
 
-void cuantosNodosHay() 
+void cuantosNodosHay()
 {
     printf("Hay ");
     printf("%d", contador);
     printf(" elementos en la lista");
-
-}
-
-void verElemento(int elemento){
-    printf("%d",agregarALaLista(elemento));
 }
 
 int main(int argc, char const *argv[])
